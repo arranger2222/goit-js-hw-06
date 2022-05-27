6fe192b616f365610bbcc215aee565acd70979d6
Max Yuvkovetskiy <yuvkovetskyy.maxim@gmail.com>
2022-05-27 19:52:10 +0300

    task 8 - ready

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event){
+    event.preventDefault();
+    const { 
+    elements: { email, password }  
+    } = event.currentTarget;
+   if(email.value === '' || password.value === ''){
+    alert('Please fill in all the required fields. ')
+}
+const userFormData = {email: email.value, password: password.value};
+console.log(userFormData);
+event.currentTarget.reset();
+};
 
