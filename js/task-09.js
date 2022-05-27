4c67608d4f77fc65740ab4092484e6de95220e4c
Max Yuvkovetskiy <yuvkovetskyy.maxim@gmail.com>
2022-05-27 22:11:10 +0300

    the ninth task is ready!

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const bodyRef = document.querySelector('body');
+const changeColorBtnRef = document.querySelector('.change-color');
+const colorValueTitleRef = document.querySelector('.color');
+
+changeColorBtnRef.addEventListener('click', handleSubmit);
+
+function handleSubmit() {
+  const newColor = getRandomHexColor();
+  colorValueTitleRef.textContent = newColor;
+  bodyRef.style.backgroundColor = newColor;
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;;
+};
