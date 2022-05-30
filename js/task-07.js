e951f0fd5a28dbc1a079a443b644b04812b80eef
Max Yuvkovetskiy <yuvkovetskyy.maxim@gmail.com>
2022-05-30 16:23:26 +0300

    update type input of 7-th task

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const rangeRef = document.querySelector('#font-size-control');
 const textRef = document.querySelector('#text');
 
-rangeRef.addEventListener('change', ()=>{
+rangeRef.addEventListener('input', ()=>{
     textRef.style.fontSize = `${rangeRef.value}px`;
     });
